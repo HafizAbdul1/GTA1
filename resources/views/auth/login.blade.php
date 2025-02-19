f911e8fbed039b1eb2b92040666755a21dcfd2ea
h6fiz <147090717+h6fiz@users.noreply.github.com>
2025-02-19 15:38:43 +0000

    made some changes, moved files to more appropriate folders.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    @vite('resources/css/desktop.css')
+
+    <!-- External Stylesheets -->
+    <link rel="stylesheet" href="{{ asset('stylesheets/desktop.css') }}">
+    @vite('resources/css/desktop.css') <!-- Ensure Vite is properly configured -->
 </head>
 
 <body>
@@ -12,38 +15,37 @@
     <div class="nav">
         <ul>
             <li class="Home"><img src="{{ asset('images/LogoGta.png') }}" alt="Logo"></li>
-            <li>Login</li>
-            <li>Example</li>
-            <li>Example</li>
+            <li><a href="{{ route('login') }}">Login</a></li>
+            <li><a href="{{ route('example.route') }}">Example</a></li>
+            <li><a href="{{ route('example.route') }}">Example</a></li>
         </ul>
     </div>
 
     <div class="login-container">
-    <form class="login-form" method="POST" action="{{ route('login.store') }}">
-        @csrf
-        <div>
-            <label for="id">Enter ID:</label><br>
-            <input type="text" id="id" name="id" class="id-password" required><br>
-        </div>
-        
-        <div>
-            <label for="password">Enter Password:</label><br>
-            <input type="password" id="password" name="password" class="id-password" required><br>
-        </div>
-
-        @if($errors->any())
-            <div class="error-messages">
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+        <form class="login-form" method="POST" action="{{ route('login.store') }}">
+            @csrf
+            <div>
+                <label for="id">Enter ID:</label><br>
+                <input type="text" id="id" name="id" class="id-password" required><br>
             </div>
-        @endif
 
-        <button type="submit" class="login-button">Login</button>
-    </form>
+            <div>
+                <label for="password">Enter Password:</label><br>
+                <input type="password" id="password" name="password" class="id-password" required><br>
+            </div>
 
+            @if($errors->any())
+                <div class="error-messages">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <button type="submit" class="login-button">Login</button>
+        </form>
     </div>
 
     <footer>
