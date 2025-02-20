b8b80f4d702ed76aa315ac662a7d4b158e8c4b7b
Danyal-Choudhry <dchoudhry2425@gmail.com>
2025-02-20 14:58:47 +0000

    updated js link

--- resources/views/profile/AdminSection/admin.blade.php
+++ resources/views/profile/AdminSection/admin.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     @vite(['resources/css/admin.css']) <!-- Vite CSS link -->
+    @vite(['resources/js/sidemenu.js']) <!-- Vite JS link for Sidemenu -->
 </head>
 <body>
 
